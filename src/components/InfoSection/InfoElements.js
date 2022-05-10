639df0055a90b99e69814defc17ece0ddd8e3118
Sean Encarnacion <seankennethencarnacion27@gmail.com>
2022-05-10 14:21:17 +0800

    color

--- src/components/InfoSection/InfoElements.js
+++ src/components/InfoSection/InfoElements.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const InfoContainer = styled.div`
     color: #fff;
-    background: ${({lightBg}) => (lightBg ? '#f9f9f9' : '#010606')};
+    background: ${({lightBg}) => (lightBg ? '#f9f9f9' : '#82E0AA')};
 
     @media screen and (max-width: 768px) {
         padding: 100px 0;
--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -3,7 +3,7 @@ import {Link as LinkR} from 'react-router-dom'
 import {Link as LinkS} from 'react-scroll'
 
 export const Nav = styled.nav`
-    background: ${({ scrollNav }) => (scrollNav ? '#000' : '#000')};
+    background: ${({ scrollNav }) => (scrollNav ? 'rgba(0, 0, 0, 0.5)' : 'rgba(0, 0, 0, 0.5)')};
     height: 80px;
     margin-top: -80px;
     display: flex;
